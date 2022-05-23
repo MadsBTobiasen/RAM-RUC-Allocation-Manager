9ec5dccee715316489ff30dd1570bb9b96b426a5
Mads <Mads@DESKTOP-R1NEBSS>
2022-05-23 10:31:39 +0200

    Changess

---
+++ Migrations/20220523081233_RAM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RAM___RUC_Allocation_Manager.Migrations
+{
+    public partial class RAM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Pages/SettingsPage/SettingsPage.cshtml.cs
+++ Pages/SettingsPage/SettingsPage.cshtml.cs
@@ -38,13 +38,10 @@ namespace RAM___RUC_Allocation_Manager.Pages.SettingsPage
             set { _leaders = value; }
         }
         public List<User> Users { get; set; }
-
-        public EmployeeProgramme NewEmployeeProgramme { get; set; }
-
         public DbService<LeaderProgramme> dbService { get; set; }
         public DbService<Programme> dbService2 { get; set; }
         public DbService<EmployeeProgramme> dbService3 { get; set; }
-        public DbService<Leader> dbService2 { get; set; }
+        public DbService<Leader> dbService4 { get; set; }
         public UserService userService { get; set; }
 
         public List<BaseSettings> BaseSettingsList { get; set; }
@@ -53,15 +50,20 @@ namespace RAM___RUC_Allocation_Manager.Pages.SettingsPage
         public BaseSettings BaseSettings { get; set; }
 
 
-        public SettingsPageModel(SettingsService settingsService, UserService userService, DbService<LeaderProgramme> dbService, DbService<Leader> dbService2)
+        public SettingsPageModel(SettingsService settingsService, UserService userService, DbService<LeaderProgramme> dbService, DbService<Programme> dbservice2, DbService<EmployeeProgramme> dbservice3, DbService<Leader> dbService4)
         {
+
             this.settingsService = settingsService;
-            this.dbService = dbService;
-            this.dbService2 = dbService2;
             this.userService = userService;
+
+            this.dbService = dbService;
+            this.dbService2 = dbservice2;
+            this.dbService3 = dbservice3;
+            this.dbService4 = dbService4;
+
+
         }
 
-        public SettingsPageModel(DbService<LeaderProgramme> dbservice, DbService<Programme> dbservice2, DbService<EmployeeProgramme> dbservice3)
         public async void OnGet()
         {
 
@@ -70,17 +72,13 @@ namespace RAM___RUC_Allocation_Manager.Pages.SettingsPage
             BaseSettingsList = settingsService.LoadSettings();
             //Leaders = await dbService2.GetObjectsAsync();
             //Users = userService.GetUsers();
-           // Leaders = userService.GetUsers();
+            //Leaders = userService.GetUsers();
             //Leaders = userService.GetUsersByType(Models.User.UserType.Leader).Cast<Leader>().ToList();
 
         }
 
         public IActionResult OnPostSettings()
         {
-        
-            dbService = dbservice;
-            dbService2 = dbservice2;
-            dbService3 = dbservice3;
             
             if (!ModelState.IsValid)
             {
